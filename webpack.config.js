241c7bc385f73f8ba553e6fe62e5bdfa2b101aee
Alexey <lexa.grydnistiy123@gmail.com>
2021-07-28 17:03:29 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "./bundle.js",
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      { test: /\.js$/, loader: "source-map-loader" },
+    ],
+  },
+  mode: "development",
+};
