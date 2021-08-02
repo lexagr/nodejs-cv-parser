602a43cb0135c242790c0ef90beeb55f1ee5a957
Alexey <lexa.grydnistiy123@gmail.com>
2021-08-02 18:09:28 +0300

    rxjs tests

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("it works");
+import { interval, Observable, Observer, of } from "rxjs";
+
+class ArrayObserver implements Observer<any> {
+  next(value: any) {
+    console.log("next: ", value);
+  }
+
+  error(err: any) {
+    console.log("error: ", err);
+  }
+
+  complete() {
+    console.log("observer end");
+  }
+}
+
+of(0, 5, 8, 2913, 4).subscribe(new ArrayObserver());
+of(0, 5, 8, 2913, 4).subscribe((val) => console.log(val));
+
+console.log("rxjs of", 0);
